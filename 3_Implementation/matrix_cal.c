244a3f11a02a60dbeaa97018f8280899cd9d2552
nimishpalod <nimish.palod12@gmail.com>
2022-02-17 21:39:09 +0530

    Final Review

--- 3_Implementation/matrix_cal.c
+++ 3_Implementation/matrix_cal.c
@@ -251,32 +251,4 @@ void determinant()
     printf("\n\nDeterminant of given matrix is: %0.3f", det);
 }
 
-#include "matrix_cal.h"
 
-int main()
-{
-    int input;
-    printf("Welcome to the world of matrix\n");
-    printf("Select the operation\n");
-    printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Transpose\n5. Determinant\n");
-    scanf("%d",&input);
-    switch(input)
-    {
-        case 1:
-            addition();
-            break;
-        case 2:
-            subtraction();
-            break;
-        case 3:
-            multiplication();
-            break;
-        case 4:
-            transpose();
-            break;
-        case 5:
-            determinant();
-            break;
-    }
-
-}
